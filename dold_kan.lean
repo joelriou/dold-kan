8f385ae9d4c9b095338b84b5dbc9227e8bc13322
Joël Riou <joel.riou@universite-paris-saclay.fr>
2021-12-25 00:23:49 +0100

    statements about the projector

--- dold_kan.lean
+++ dold_kan.lean
@@ -28,9 +28,8 @@ namespace algebraic_topology
 namespace dold_kan
 
 variables {C : Type*} [category C] [preadditive C]
-variables {X : simplicial_object C}
 
-/- general stuff of homotopies -/
+/- general stuff on homotopies -/
 
 @[simp]
 def null_homotopic_chain_complex_map_f {K L : chain_complex C ℕ}
@@ -77,11 +76,13 @@ def homotopy_of_null_homotopic_chain_complex_map {K L : chain_complex C ℕ}
 
 /- construction of homotopies -/
 
+variables {X : simplicial_object C}
+
 def hσ (q : ℕ) (n : ℕ) : X _[n] ⟶ X _[n+1] := if n<q then 0
   else (-1 : ℤ)^(n-q) • X.σ (fin.mk (n-q) (nat.sub_lt_succ n q))
 
 @[simp]
-lemma hδ_eq_zero (q : ℕ) (n : ℕ) (hnq : n<q) : (hσ q n : X _[n] ⟶ X _[n+1])= 0 :=
+lemma hσ_eq_zero (q : ℕ) (n : ℕ) (hnq : n<q) : (hσ q n : X _[n] ⟶ X _[n+1])= 0 :=
 begin
   unfold hσ,
   simp only [fin.mk_eq_subtype_mk, ite_eq_left_iff],
@@ -113,6 +114,46 @@ def P : ℕ → ((alternating_face_map_complex C).obj X ⟶
 (alternating_face_map_complex C).obj X)
 | 0     := 𝟙 _
 | (q+1) := P q ≫ (𝟙 _ - Hσ q)
+
+/- these endormorphismes P q coincide with `𝟙` in degree 0 -/
+
+lemma P_deg0_eq (q : ℕ) : ((P q).f 0 : X _[0] ⟶ X _[0]) = 𝟙 _ :=
+begin
+  induction q with q hq,
+  { simp, },
+  { simp [hq],
+    cases q,
+    { erw chain_complex.of_d,
+      simp [hσ_eq 0 0 0 (by refl), alternating_face_map_complex.obj_d],
+      rw [fin.sum_univ_succ_above, fin.sum_univ_one,
+        fin.zero_succ_above, fin.succ_zero_eq_one],
+      simp only [comp_neg, fin.coe_zero, comp_add, fin.coe_one, pow_one,
+        one_zsmul, pow_zero, neg_smul],
+      apply add_neg_eq_zero.mpr,
+      erw [δ_comp_σ_self, δ_comp_σ_succ], },
+    { simp, }, },
+end
+
+/- vanishing of some faces -/
+
+structure higher_faces_vanish {Y : C} {n : ℕ} (q : ℕ) (φ : Y ⟶ X _[n+1]) : Prop :=
+  (vanishing : ∀ (j : fin(n+1)), (n+1 ≤ (j : ℕ) + q) → φ ≫ X.δ j.succ = 0)
+
+lemma Hσφ_eq_zero {Y : C} {n : ℕ} (q : ℕ) (hqn : q<n) (φ : Y ⟶ X _[n+1])
+  (v : higher_faces_vanish q φ) : φ ≫ (Hσ q).f (n+1) = 0 := sorry
+
+lemma Hσφ_eq_σδ {Y : C} {n : ℕ} (q : ℕ) (hqn : q≤n) (φ : Y ⟶ X _[n+1])
+  (v : higher_faces_vanish q φ) : φ ≫ (Hσ q).f (n+1) = 
+  φ ≫ X.δ (fin.mk (n-q) (nat.sub_lt_succ n q)).succ ≫
+  X.σ (fin.mk (n-q) (nat.sub_lt_succ n q)) := sorry
+
+lemma higher_faces_vanish_ind {Y : C} {n : ℕ} (q : ℕ) {φ : Y ⟶ X _[n+1]} 
+  (v : higher_faces_vanish q φ) : higher_faces_vanish (q+1) (φ ≫ (𝟙 _ - Hσ q).f (n+1)) :=
+{ vanishing :=
+  begin
+      sorry
+  end
+}
 
 end dold_kan
 
