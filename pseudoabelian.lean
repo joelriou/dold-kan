eebe6d8cfc1c45a9d2a569ea39f38d7c96c2fe54
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-01-02 23:35:32 +0100

    cleaning the code

--- pseudoabelian.lean
+++ pseudoabelian.lean
@@ -32,16 +32,16 @@ def binary_bicone_of_projector {X : C} (p : X ⟶ X)
   [h : is_projector p] [has_kernel p] [has_kernel (𝟙 X - p)] :
   binary_bicone (kernel (𝟙 X - p)) (kernel p) :=
 { X := X,
+  inl := kernel.ι (𝟙 X - p),
+  inr := kernel.ι p,
   fst := kernel.lift (𝟙 X - p) p 
     (by { rw [comp_sub, category.comp_id, sub_eq_zero], exact h.idempotence.symm, }),
   snd := kernel.lift p (𝟙 X - p)
     (by { rw [sub_comp, category.id_comp, sub_eq_zero], exact h.idempotence.symm, }),
-  inl := kernel.ι (𝟙 X - p),
-  inr := kernel.ι p,
   inl_snd' := by { dsimp, ext, simp only [limits.zero_comp, limits.kernel.condition,
-      category.assoc, limits.kernel.lift_ι], },
+    category.assoc, limits.kernel.lift_ι], },
   inr_fst' := by { dsimp, ext, simp only [limits.zero_comp, limits.kernel.condition,
-      category.assoc, limits.kernel.lift_ι], },
+    category.assoc, limits.kernel.lift_ι], },
   inl_fst' := sorry,
   inr_snd' := sorry,
 }
