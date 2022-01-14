d0519c0dc5a52ea22f542047445a0b7926dc0158
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-01-14 22:44:05 +0100

    karoubi_simplicial_object_functor is fully faithful

--- pseudoabelian/simplicial_object.lean
+++ pseudoabelian/simplicial_object.lean
@@ -32,27 +32,23 @@ namespace category_theory
 
 namespace pseudoabelian
 
-namespace karoubi_simplicial_object
+namespace karoubi_simplicial_object_functor
 
-namespace functor
-
-abbreviation obj (P : karoubi (simplicial_object C)) : simplicial_object (karoubi C) :=
-{ obj := λ Δ, ⟨P.X.obj Δ, P.p.app Δ,
-    congr_arg ((λ f, f.app Δ) : (P.X ⟶ P.X) → (P.X.obj Δ ⟶ P.X.obj Δ)) P.idempotence⟩,
+@[simps]
+def obj (P : karoubi (simplicial_object C)) : simplicial_object (karoubi C) :=
+{ obj := λ Δ, ⟨P.X.obj Δ, P.p.app Δ, congr_app P.idempotence Δ⟩,
   map := λ Δ Δ' θ,
   { f := P.p.app Δ ≫ P.X.map θ,
     comm := begin
       simp only [nat_trans.naturality, assoc],
-      have h := congr_arg ((λ f, f.app Δ) : (P.X ⟶ P.X) → (P.X.obj Δ ⟶ P.X.obj Δ))
-        P.idempotence,
+      have h := congr_app P.idempotence Δ,
       dsimp at h,
       slice_rhs 1 3 { rw [h, h], },
     end, },
   map_id' := λ Δ, by { ext, simp only [P.X.map_id Δ, comp_id, karoubi.id_eq], },
   map_comp' := λ Δ Δ' Δ'' f g, begin
     ext, 
-    have h := congr_arg ((λ f, f.app Δ) : (P.X ⟶ P.X) → (P.X.obj Δ ⟶ P.X.obj Δ))
-      P.idempotence,
+    have h := congr_app P.idempotence Δ,
     dsimp at h,
     simp only [assoc, nat_trans.naturality_assoc, functor.map_comp,
       karoubi.comp],
@@ -60,34 +56,55 @@ abbreviation obj (P : karoubi (simplicial_object C)) : simplicial_object (karoub
     rw [assoc],
   end }
 
-abbreviation map {P Q : karoubi (simplicial_object C)} (f : P ⟶ Q) : obj P ⟶ obj Q :=
-{ app := λ Δ, ⟨f.f.app Δ, congr_arg ((λ f, f.app Δ) :
-    (P.X ⟶ Q.X) → (P.X.obj Δ ⟶ Q.X.obj Δ)) f.comm⟩,
+@[simps]
+def map {P Q : karoubi (simplicial_object C)} (f : P ⟶ Q) : obj P ⟶ obj Q :=
+{ app := λ Δ, ⟨f.f.app Δ, congr_app f.comm Δ⟩,
   naturality' := λ Δ Δ' θ, begin
     ext,
     simp only [karoubi.comp],
-    have h := congr_arg ((λ f, f.app Δ) : (P.X ⟶ Q.X) → (P.X.obj Δ ⟶ Q.X.obj Δ))
-      (karoubi.comp_p f),
-    have h' := congr_arg ((λ f, f.app Δ') : (P.X ⟶ Q.X) → (P.X.obj Δ' ⟶ Q.X.obj Δ'))
-      (karoubi.p_comp f),
-    dsimp at h h',
+    have h := congr_app (karoubi.comp_p f) Δ,
+    have h' := congr_app (karoubi.p_comp f) Δ',
+    dsimp at h h' ⊢,
     slice_rhs 1 2 { erw h, },
     rw ← P.p.naturality,
     slice_lhs 2 3 { erw h', },
     rw f.f.naturality,
   end }
 
-end functor
+end karoubi_simplicial_object_functor
 
-def functor : karoubi (simplicial_object C) ⥤ simplicial_object (karoubi C) :=
-{ obj := functor.obj,
-  map := λ P Q f, functor.map f,
+variables (C)
+
+@[simps]
+def karoubi_simplicial_object : karoubi (simplicial_object C) ⥤ simplicial_object (karoubi C) :=
+{ obj := karoubi_simplicial_object_functor.obj,
+  map := λ P Q f, karoubi_simplicial_object_functor.map f,
   map_id' := λ P,
-    by { ext Δ, simp only [nat_trans.id_app, karoubi.id_eq], },
+    by { ext Δ, simp only [nat_trans.id_app, karoubi.id_eq], dsimp, refl, },
   map_comp' := λ P Q R f f',
-    by { ext Δ, simp only [karoubi.comp, nat_trans.comp_app], }, }
+    by { ext Δ, simp only [karoubi.comp, nat_trans.comp_app], dsimp, refl, }, }
 
-end karoubi_simplicial_object
+instance : full (karoubi_simplicial_object C) :=
+{ preimage := λ P Q f,
+  { f :=
+    { app := λ Δ, (f.app Δ).f,
+      naturality' := λ Δ Δ' θ, begin
+        slice_rhs 1 1 { rw ← karoubi.comp_p, },
+        have h := karoubi.hom_ext.mp (f.naturality θ),
+        simp only [karoubi.comp] at h,
+        dsimp at h ⊢,
+        erw [assoc, ← h, ← P.p.naturality θ, assoc, karoubi.p_comp (f.app Δ')],
+      end },
+    comm := by { ext Δ, exact (f.app Δ).comm, } },
+  witness' := λ P Q f, by { ext Δ, dsimp, refl, }, }
+
+instance : faithful (karoubi_simplicial_object C) :=
+{ map_injective' := λ P Q f f' h, begin
+    ext Δ,
+    simp only [karoubi_simplicial_object_functor.map, nat_trans.ext_iff,
+      karoubi_simplicial_object_map] at h,
+    simpa only using congr_fun h Δ,
+  end, }
 
 end pseudoabelian
 
