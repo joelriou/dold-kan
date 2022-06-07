9d7063d0b844360d4c0e60acd2c4632082635cf5
Joël Riou <joel.riou@universite-paris-saclay.fr>
2022-06-08 01:19:33 +0200

    tidied the code

--- src/for_mathlib/simplex_category/simplex_category2.lean
+++ src/for_mathlib/simplex_category/simplex_category2.lean
@@ -1,7 +1,5 @@
 import algebraic_topology.simplex_category
 import category_theory.limits.shapes.images
-import category_theory.limits.shapes.strong_epi
---import simplex_category.simplex_category1
 
 universes u v
 
@@ -18,16 +16,11 @@ def strong_epi_of_epi {X Y : simplex_category} (f : X ⟶ Y) [epi f] :
     have comm' := λ (x : fin (X.len+1)), congr_arg (λ F, hom.to_order_hom F x) comm,
     simp only [hom.to_order_hom_mk, function.comp_app, order_hom.comp_coe,
       hom.comp, small_category_comp] at comm',
-    let p : fin (Y.len+1) → set (fin (X.len+1)) := λ y x, f.to_order_hom x = y,
-    have exists_lift : ∀ (y : fin (Y.len+1)),
-      ∃ (x : fin (X.len+1)), f.to_order_hom x = y := λ y,
-    by { cases epi_iff_surjective.mp (show epi f, by apply_instance) y with a ha,
-      use a, exact ha, },
-    let lift : fin (Y.len+1) → fin (X.len+1) := λ y, classical.some (exists_lift y),
-    have hlift : ∀ (y : fin (Y.len+1)), f.to_order_hom (lift y) = y :=
-      λ y, classical.some_spec (exists_lift y),
-    let γ' : fin (Y.len+1) → fin (A.len+1) := λ y, u.to_order_hom (lift y),
-    have hγ' : ∀ (x : fin (X.len+1)), γ' (f.to_order_hom x) = u.to_order_hom x,
+    have surjectivity := epi_iff_surjective.mp (infer_instance : epi f),
+    let lift := function.inv_fun f.to_order_hom,
+    have hlift : ∀ y, f.to_order_hom (lift y) = y := function.right_inverse_inv_fun surjectivity,
+    let γ' := λ y, u.to_order_hom (lift y),
+    have hγ' : ∀ x, γ' (f.to_order_hom x) = u.to_order_hom x,
     { intro x,
       apply mono_iff_injective.mp hw,
       simp only [comm', hlift], },
@@ -36,19 +29,17 @@ def strong_epi_of_epi {X Y : simplex_category} (f : X ⟶ Y) [epi f] :
         monotone' := λ y₁ y₂ h, begin
           cases eq_or_lt_of_le h with h' h',
           { rw h', },
-          { apply (hom.to_order_hom u).monotone',
+          { apply (hom.to_order_hom u).monotone,
             by_contradiction h'',
-            have ineq := (hom.to_order_hom f).monotone' (le_of_lt (not_le.mp h'')),
+            have ineq := (hom.to_order_hom f).monotone (le_of_lt (not_le.mp h'')),
             simp only [hlift, order_hom.to_fun_eq_coe] at ineq,
             rw [le_antisymm h ineq] at h',
             exact (irrefl y₂ : ¬(y₂<y₂)) h', },
         end, },
-    refine ⟨nonempty.intro { lift := γ, fac_left' := _, fac_right' := _, }⟩,
-    { ext1, ext1, ext1 x,
-      exact hγ' x, },
-    { ext y,
-      dsimp,
-      rw [← hlift y, hγ', comm'], },
+    exact ⟨nonempty.intro
+    { lift := γ,
+      fac_left' := by { ext1, ext1, ext1, exact hγ' x, },
+      fac_right' := by { ext y, dsimp, rw [← hlift y, hγ', comm'], }, }⟩,
   end }
 
 def strong_epi_mono_factorisation_of_epi_mono_factorisation
@@ -56,34 +47,28 @@ def strong_epi_mono_factorisation_of_epi_mono_factorisation
   [epi e] [mono i] (h : e ≫ i = f) : strong_epi_mono_factorisation f :=
 begin
   haveI : strong_epi e := strong_epi_of_epi e,
-  exact
-{ I := y,
-  m := i,
-  e := e,
-  m_mono := by apply_instance,
-  fac' := h, },
+  exact { I := y, m := i, e := e, m_mono := by apply_instance, fac' := h, },
 end
 
-def canonical_strong_epi_mono_factorisation {x y : simplex_category} (f : x ⟶ y) :
-  strong_epi_mono_factorisation f :=
+instance : has_strong_epi_mono_factorisations simplex_category :=
 begin
-  let α := { j : fin(y.len+1) // ∃ (i : fin(x.len+1)), f.to_order_hom i = j },
+  constructor,
+  intros x y f,
+  let α := { j // ∃ i, f.to_order_hom i = j },
+  let ψ : order_hom _ α :=
+    ⟨λ i, ⟨f.to_order_hom i, ⟨i, rfl⟩⟩, λ _ _ h, f.to_order_hom.monotone h⟩,
   let n := fintype.card α-1,
-  have eq : fintype.card α = n+1 := begin
-    by_cases fintype.card α ≥ 1,
-    { exact (nat.sub_eq_iff_eq_add h).mp rfl, },
+  have eq : fintype.card α = n+1,
+  { cases nat.eq_zero_or_eq_succ_pred (fintype.card α),
     { exfalso,
-      simp only [not_le, nat.lt_one_iff, fintype.card_eq_zero_iff] at h,
+      rw fintype.card_eq_zero_iff at h,
       apply h.false,
-      exact ⟨f.to_order_hom 0, (by use 0)⟩, },
-  end,
-  let ψ : order_hom (fin (x.len+1)) α := ⟨λ i, ⟨f.to_order_hom i, by use i⟩,
-    λ _ _ h, f.to_order_hom.monotone' h⟩,
+      exact ⟨f.to_order_hom 0, ⟨0, rfl⟩⟩, },
+    { exact h, }, },
   let φ := mono_equiv_of_fin α eq,
-  let γ : order_hom α (fin (y.len+1)) := ⟨λ j, j.1,
-    by { rintros ⟨i₁,_⟩ ⟨i₂,_⟩ h, simpa only using h, }⟩,
+  let γ : order_hom α (fin (y.len+1)) := ⟨λ j, j.1, λ _ _ h, h, ⟩,
   let e : x ⟶ mk n := hom.mk (order_hom.comp
-      (order_embedding.to_order_hom (order_iso.to_order_embedding φ.symm)) ψ),
+    (order_embedding.to_order_hom (order_iso.to_order_embedding φ.symm)) ψ),
   haveI : epi e,
   { apply epi_iff_surjective.mpr,
     intro k,
@@ -94,29 +79,20 @@ begin
       order_hom.comp_coe, order_hom.coe_fun_mk,
       order_iso.symm_apply_apply, subtype.coe_eta, subtype.val_eq_coe], },
   haveI : strong_epi e := strong_epi_of_epi e,
-  exact
+  exact nonempty.intro
   { I := mk n,
     m := hom.mk (order_hom.comp
       γ (order_embedding.to_order_hom (order_iso.to_order_embedding φ))),
     e := e,
     m_mono := begin
-      apply mono_iff_injective.mpr,
+      rw mono_iff_injective,
       intros i₁ i₂ h,
-      dsimp,
       simp only [hom.to_order_hom_mk, order_iso.coe_to_order_embedding,
         order_embedding.to_order_hom_coe,function.comp_app,  order_hom.comp_coe,
         subtype.val_eq_coe, order_hom.coe_fun_mk, ← subtype.ext_iff] at h,
       exact equiv.injective φ.to_equiv h,
-    end,
-    fac' := by { ext i, dsimp,
-      simpa only [order_iso.apply_symm_apply, hom.to_order_hom_mk,
-        order_iso.coe_to_order_embedding, order_embedding.to_order_hom_coe,
-        function.comp_app, order_hom.comp_coe, order_hom.coe_fun_mk], }, },
+    end, },
 end
-
-instance : has_strong_epi_mono_factorisations simplex_category :=
-  has_strong_epi_mono_factorisations.mk
-  (λ _ _ f, canonical_strong_epi_mono_factorisation f)
 
 lemma image_eq {Δ Δ' Δ'' : simplex_category } {φ : Δ ⟶ Δ''}
   {e : Δ ⟶ Δ'} [epi e] {i : Δ' ⟶ Δ''} [mono i] (fac : e ≫ i = φ) :
